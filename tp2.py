d263fae3a50ba52e80002199fddd1c17dcde5427
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-10-15 16:25:57 -0300

    Add files via upload

---
+++ tp2.py
@@ -0,0 +1,19 @@
+first_name = "AdA"
+last_name  = "LoVeLAce"
+print(first_name.lower()+ " " + last_name.lower())
+print(first_name.title() + " " + last_name.title())
+print(first_name.upper() + " " + last_name.upper())
+
+x="Bangladesh"
+y="Barbados"
+l=x<y
+n=x>y
+print(f"The result of {x} comes first in the dictionary than {y} is {n}.")
+print(f"The result of {y} comes first in the dictionary than {x} is {l}.")
+
+t=float(input("cuanto tiene que pagar: "))
+p=float(input("con cuanto paga: "))
+b= p-t
+c=int(b)
+print(f"centavos: {b-c}")
+print(f"pesos: {b}")
